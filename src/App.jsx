d0e79fc7ef038b24d098ec137f35a8e7c03eea9e
Daniel Ruiz <danielruimar@gmail.com>
2024-04-18 01:35:02 +0200

    Fix code and change color login button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import Locks from './pages/Locks';
-import Loading from './components/Loading';
 import LockDetail from './pages/LockDetail';
 import Login from './pages/Login';
 
-import { Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <HashRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/locks" element={<Locks />} />
-          <Route path="/lock/:id" element={<LockDetail />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/locks" element={<Locks />} />
+        <Route path="/lock/:id" element={<LockDetail />} />
+      </Routes>
     </HashRouter>
   );
 };
